69adc646fc47f3ed03b9cd2fa072d05875659a22
shannu <mendegoutham@gmail.com>
2024-03-10 11:18:23 +0530

    industries

---
+++ GouthamindustriesApplication.java
@@ -0,0 +1,12 @@
+package com.goutham.industries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GouthamIndustriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GouthamIndustriesApplication.class, args);
+    }
+}
---
+++ UserService.java
@@ -0,0 +1,8 @@
+package com.goutham.industries.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    // Implement user-related services as needed
+}
